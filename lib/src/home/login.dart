87110d179b28a07c5aa02b11c6abfea0b7530300
Raja Thirumal Govindaraj <rajathirumal98@gmail.com>
2022-03-19 00:41:49 +0530

    Added foem to Fix and close #1

--- lib/src/home/login.dart
+++ lib/src/home/login.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fuel_tracker/services/authentication.dart';
-import 'package:fuel_tracker/src/home/home.dart';
 import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
--- lib/src/pages/add_fuel_page.dart
+++ lib/src/pages/add_fuel_page.dart
@@ -59,6 +59,7 @@ class _AddFuelPageState extends State<AddFuelPage> {
   @override
   Widget build(BuildContext context) {
     final fuelProvider = Provider.of<FuelProvider>(context);
+    final _fuelFormKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -73,57 +74,135 @@ class _AddFuelPageState extends State<AddFuelPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: ListView(
-          children: <Widget>[
-            TextField(
-              controller: fuelForTEC,
-              decoration: const InputDecoration(hintText: "Fueled for (Rs)"),
-              keyboardType: TextInputType.number,
-              onChanged: (value) => fuelProvider.changeFuelForPrice(value),
-            ),
-            TextField(
-              controller: marketPriceTEC,
-              decoration:
-                  const InputDecoration(hintText: "Market price (Rs/liter)"),
-              keyboardType: TextInputType.number,
-              onChanged: (value) => fuelProvider.changemarketPrice(value),
-            ),
-            TextField(
-              controller: atKmTEC,
-              decoration: const InputDecoration(hintText: "At Km (Km)"),
-              keyboardType: TextInputType.number,
-              onChanged: (value) => fuelProvider.changeAtKm(value),
-            ),
-            TextField(
-              controller: remainingKmTEC,
-              decoration: const InputDecoration(hintText: "Remaining KM (Km)"),
-              keyboardType: TextInputType.number,
-              onChanged: (value) => fuelProvider.changeRemainingKm(value),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-                onPressed: () {
-                  fuelProvider.saveFuelToFireStore(
-                    fuelID:
-                        widget.fuel == null ? Uuid().v1() : widget.fuel!.fuelID,
-                    fuelforprice: fuelForTEC.text,
-                    marketprice: marketPriceTEC.text,
-                    atkms: atKmTEC,
-                    remainingkms: remainingKmTEC.text,
-                    daterefueld: widget.fuel == null
-                        ? DateTime.now().toString()
-                        : widget.fuel!.dateOfFuel,
-                  );
-                  Navigator.of(context).pop();
+        child: Form(
+          key: _fuelFormKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(hintText: "Refueled for"),
+                controller: fuelForTEC,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter money spent';
+                  }
+                  return null;
                 },
-                child: const Text("Save")),
-            const SizedBox(height: 10),
-            ElevatedButton(
-                onPressed: () => fuelProvider.removeFuelFromFireStore(),
-                child: const Text("Delete")),
-          ],
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: "Price per liter"),
+                controller: marketPriceTEC,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter cost per liter';
+                  } else {
+                    null;
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: "Refueld at KM"),
+                controller: atKmTEC,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter current oodometer reading';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: "Remaining KM"),
+                controller: remainingKmTEC,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter remaining KM';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_fuelFormKey.currentState!.validate()) {}
+                },
+                child: const Text(
+                  'Add fuel',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text(
+    //       "ADD FUEL",
+    //       style: TextStyle(
+    //         color: Colors.white70,
+    //         fontWeight: FontWeight.bold,
+    //         letterSpacing: 3,
+    //       ),
+    //     ),
+    //     centerTitle: true,
+    //   ),
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(20.0),
+    //     child: ListView(
+    //       children: <Widget>[
+    //         TextField(
+    //           controller: fuelForTEC,
+    //           decoration: const InputDecoration(hintText: "Fueled for (Rs)"),
+    //           keyboardType: TextInputType.number,
+    //           onChanged: (value) => fuelProvider.changeFuelForPrice(value),
+    //         ),
+    //         TextField(
+    //           controller: marketPriceTEC,
+    //           decoration:
+    //               const InputDecoration(hintText: "Market price (Rs/liter)"),
+    //           keyboardType: TextInputType.number,
+    //           onChanged: (value) => fuelProvider.changemarketPrice(value),
+    //         ),
+    //         TextField(
+    //           controller: atKmTEC,
+    //           decoration: const InputDecoration(hintText: "At Km (Km)"),
+    //           keyboardType: TextInputType.number,
+    //           onChanged: (value) => fuelProvider.changeAtKm(value),
+    //         ),
+    //         TextField(
+    //           controller: remainingKmTEC,
+    //           decoration: const InputDecoration(hintText: "Remaining KM (Km)"),
+    //           keyboardType: TextInputType.number,
+    //           onChanged: (value) => fuelProvider.changeRemainingKm(value),
+    //         ),
+    //         const SizedBox(height: 10),
+    //         ElevatedButton(
+    //             onPressed: () {
+    //               fuelProvider.saveFuelToFireStore(
+    //                 fuelID:
+    //                     widget.fuel == null ? Uuid().v1() : widget.fuel!.fuelID,
+    //                 fuelforprice: fuelForTEC.text,
+    //                 marketprice: marketPriceTEC.text,
+    //                 atkms: atKmTEC,
+    //                 remainingkms: remainingKmTEC.text,
+    //                 daterefueld: widget.fuel == null
+    //                     ? DateTime.now().toString()
+    //                     : widget.fuel!.dateOfFuel,
+    //               );
+    //               Navigator.of(context).pop();
+    //             },
+    //             child: const Text("Save")),
+    //         const SizedBox(height: 10),
+    //         ElevatedButton(
+    //             onPressed: () => fuelProvider.removeFuelFromFireStore(),
+    //             child: const Text("Delete")),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
