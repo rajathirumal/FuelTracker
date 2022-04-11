f5f3bea545c36409bc340e0e04d3c3a6cb1ff40a
Raja Thirumal Govindaraj <rajathirumal98@gmail.com>
2022-04-11 18:47:52 +0530

    prompting error when invalid user

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -16,6 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController passController = TextEditingController();
 
   final auth = FirebaseAuth.instance;
+  bool loginError = false;
 
   final _formKey = GlobalKey<FormState>();
   @override
@@ -41,88 +42,160 @@ class _LoginScreenState extends State<LoginScreen> {
                   "Sign in to continue",
                   style: TextStyle(fontSize: 25.0),
                 ),
-                const SizedBox(height: 50),
+                const SizedBox(height: 30),
+                Visibility(
+                  visible: loginError,
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.red.withOpacity(0.3),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.error_outline_outlined,
+                                size: 40,
+                                color: Colors.redAccent,
+                              ),
+                              const SizedBox(width: 20.0),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    "Invalid user:",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontStyle: FontStyle.italic,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    "Kindly Check you creadentials ...",
+                                    style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      fontSize: 15,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                ),
                 Form(
                   key: _formKey,
-                  child: Column(children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        border: Border.all(
-                          color: Colors.black26,
-                          width: 1.0,
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10),
-                        child: TextFormField(
-                          // The validator receives the text that the user has entered.
-                          keyboardType: TextInputType.emailAddress,
-                          controller: emailController,
-                          decoration: const InputDecoration(
-                            hintText: "Email",
-                            hintStyle:
-                                TextStyle(fontSize: 20, color: Colors.black),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          border: Border.all(
+                            color: Colors.black26,
+                            width: 1.0,
                           ),
-                          validator: (value) {
-                            if (value == null ||
-                                value.isEmpty ||
-                                !value.emailRegex) {
-                              return 'Please valid email.(Example: user@domain.com)';
-                            }
-                            return null;
-                          },
                         ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        border: Border.all(
-                          color: Colors.black26,
-                          width: 1.0,
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10),
-                        child: TextFormField(
-                          obscureText: true,
-                          controller: passController,
-                          decoration: const InputDecoration(
-                            hintText: "Password",
-                            hintStyle:
-                                TextStyle(fontSize: 20, color: Colors.black),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: TextFormField(
+                            // The validator receives the text that the user has entered.
+                            keyboardType: TextInputType.emailAddress,
+                            controller: emailController,
+                            decoration: const InputDecoration(
+                              hintText: "Email",
+                              hintStyle:
+                                  TextStyle(fontSize: 20, color: Colors.black),
+                            ),
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  !value.emailRegex) {
+                                return 'Please valid email.(Example: user@domain.com)';
+                              }
+                              return null;
+                            },
                           ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter password';
-                            } else if (value.length < 6) {
-                              return "Password should be atlest 6 charecters";
-                            }
-                            return null;
-                          },
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    ElevatedButton.icon(
-                      style:
-                          ButtonStyle(elevation: MaterialStateProperty.all(20)),
-                      icon: const Icon(Icons.login, color: Colors.white),
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          context.read<AuthenticationService>().signIn(
-                              email: emailController.text,
-                              password: passController.text);
-                        }
-                      },
-                      label: const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      const SizedBox(height: 20),
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          border: Border.all(
+                            color: Colors.black26,
+                            width: 1.0,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: TextFormField(
+                            obscureText: true,
+                            controller: passController,
+                            decoration: const InputDecoration(
+                              hintText: "Password",
+                              hintStyle:
+                                  TextStyle(fontSize: 20, color: Colors.black),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter password';
+                              } else if (value.length < 6) {
+                                return "Password should be atlest 6 charecters";
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
                       ),
-                    ),
-                  ]),
+                      const SizedBox(height: 20.0),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton.icon(
+                          style: ButtonStyle(
+                              elevation: MaterialStateProperty.all(20)),
+                          icon: const Icon(Icons.login, color: Colors.white),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              context
+                                  .read<AuthenticationService>()
+                                  .signIn(
+                                    email: emailController.text,
+                                    password: passController.text,
+                                  )
+                                  .then((value) {
+                                if (value != "signed in") {
+                                  _formKey.currentState!.reset();
+                                  setState(() async {
+                                    setState(() {
+                                      loginError = true;
+                                    });
+                                    await Future.delayed(
+                                        const Duration(seconds: 3), () {
+                                      setState(() {
+                                        loginError = false;
+                                      });
+                                    });
+                                  });
+                                }
+                              });
+                            }
+                          },
+                          label: const Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
